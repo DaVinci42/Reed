b34548dea7f6cd4de954d8301de0eff403c82a32
Dan Xin <starrynight.ying@gmail.com>
2015-10-27 18:51:02 +0800

    Regular Update

---
+++ app/src/main/java/io/github/davinci/seed/MvpBase/MvpPresenter.java
@@ -0,0 +1,11 @@
+package io.github.davinci.seed.MvpBase;
+
+/**
+ * Created by davinci42 on 15/10/27.
+ */
+public interface MvpPresenter<V extends MvpView> {
+
+    void attachView(V view);
+
+    void detachView(boolean retainInstance);
+}
