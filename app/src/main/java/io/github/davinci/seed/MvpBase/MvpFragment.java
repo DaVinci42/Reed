1583abd06d07ab4f1c1935f9c23eca0b96704d43
Dan Xin <starrynight.ying@gmail.com>
2015-11-05 11:32:52 +0800

    Fix conflict

--- app/src/main/java/io/github/davinci/seed/MvpBase/MvpFragment.java
+++ app/src/main/java/io/github/davinci/seed/MvpBase/MvpFragment.java
@@ -1,12 +1,9 @@
 package io.github.davinci.seed.MvpBase;
 
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.View;
 
 public abstract class MvpFragment<V extends CoreView, P extends CorePresenter<V>> extends CoreFragment<V, P> implements MvpView{
+
 
 
 }
